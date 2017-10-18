a8c9af2f57c8f6acc269c0eb9ed673a5190cc255
Guy Nankivell <gnan075@aucklanduni.ac.nz>
2017-10-19 09:30:36 +1300

    Reorganised?

--- reverselist.c
+++ reverselist.c
@@ -1,8 +1,16 @@
-/* This compilation unit includes all the content that pertains to 
- * initialising and manipulating relationships betweeen vertices */
 
-#include "reverselist.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "vertex.h"
 
+
+#define LINE_SIZE               128
+#define FILE_PATH               "./list.txt"
+#define FILE_OUT                "out.txt"
+
+int main(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
@@ -28,153 +36,3 @@ int main(int argc, char** argv)
 
         return 0;
 }
-
-/* This function breathes life into our vertex structure. We give it a 
- * unique vertex id, and pass in how many possible elements in the 
- * adjacency list for each vertex. The safest thing to do here is pass in
- * the total number of elements in the adjacency matrix as it could 
- * be at max adjacent to that many elements. We also keep track of 
- * whether or not the arc has been reversed in the past */
-Vertex initVertex(uint32_t vid, uint32_t n)
-{
-        Vertex v = malloc(sizeof(Vertex));
-
-        v->id = vid;
-        v->adjacent = malloc(n * sizeof(Vertex));
-        v->count = 0;
-        v->eletotal = n;
-        v->reversed = 0;
-
-        return v;
-
-}
-
-
-/* This function adds the adjacent vertex `adj` to the adjacency list
- * of vertex v */
-void addAdjacent(Vertex v, Vertex adj)
-{
-        /* Now that we have trivial edge cases out of the way... */
-        if(v->count == v->eletotal){
-                return;
-        }
-        
-        (v->adjacent)[v->count] = adj;
-        v->count++; 
-        return;
-
-}
-
-/* We reverse an arc when there is a directed arc pointing at one vertex
- * but the vertex does not return the love. 
- *
- * Improve nomenclature 
- *
- * for example:
- *
- * v ----> adj
- *
- * if adj is in v's adjacency list, we know too that there is an arc 
- * between the two at least. If in adj's adjacency list we find v, then
- * this implies:
- *
- * v <---> adj
- *
- * And in this case we do nothing.
- *
- * */
-void reverseArcs(Vertex v)
-{
-        Vertex* arr = (v->adjacent);
-
-        for(uint32_t i = 0; i < v->count; i++){
-
-                if(!arr[i]){ 
-                        continue;
-                }
-
-                Vertex adj = arr[i];
-
-                /* An arc is reversible either when neither are reversed
-                 * or both are reversed. We can check this thusly: */
-//                uint32_t rev = !(adj->reversed ^ v->reversed);
-                uint32_t rev = !(v->reversed) || adj->reversed;
-
-                /* Then we reverse */
-                if(rev && !containsVertex(v, adj)){
-                                addAdjacent(adj, v);
-                                removeAdjacent(v, adj);
-                }
-
-        }
-
-        v->reversed = !(v->reversed);
-
-}
-
-
-/* Checks if vertex v is in the adjacency list of adj */
-int containsVertex(Vertex v, Vertex adj)
-{
-        Vertex* tmp = (adj->adjacent);
-
-        for(uint32_t i = 0; i < adj->count; i++){
-
-                if(v == tmp[i]){
-                        return 1;
-                }
-        }
-
-        return 0;
-
-}
-
-
-/* This function removes the adjacent vertex `adj` from the adjacency list
- * of vertex v */
-void removeAdjacent(Vertex v, Vertex adj)
-{
-        Vertex* tmp = (v->adjacent);
-
-        /* HOLD UP DO I KEEP COUNT THE SAME */
-
-        for(uint32_t i = 0; i < v->count; i++){
-
-                if(!tmp[i]){
-                        continue;
-                }
-
-                if(tmp[i] == adj){
-                        tmp[i] = NULL; 
-                }
-
-        }
-
-        return;
-
-}
-
-
-/* Simply prints the adjacency list of vertex v. Example output:
- *
- * 2: 3 5 6
- *
- * Which designates that vertex 2 (v) is adjacent to vertices 3, 5, 6 */
-void printAdjacent(Vertex v)
-{
-        Vertex* tmp = v->adjacent;
-
-        printf("%"PRIu32": ", v->id);
-
-        for(uint32_t i = 0; i < v->count; i++){
-
-                if(!tmp[i]) continue;
-
-                printf("%"PRIu32" ", (tmp[i])->id);
-
-        }
-
-        printf("\n");
-
-}
-
---
+++ vertex.h
@@ -0,0 +1,35 @@
+
+
+#ifndef         __VERTEX_220_H__
+#define         __VERTEX_220_H__
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+
+/* Contains a distinguishing id (for human readable purposes), pointer
+ * to an array of other vertices that this vertex is adjacent to, and
+ * a count that keeps track of how many vertices are contained in the
+ * array of adjacent vertices. The amount of vertices that are allocated
+ * for are also stored in eletotal; read: element total */
+typedef struct _vertex {
+        uint32_t id;
+        struct _vertex** adjacent;
+        uint32_t count;
+        uint32_t eletotal;
+        uint16_t reversed;
+} *Vertex;
+
+
+/* Vertex compilation unit function prototypes */
+Vertex initVertex(uint32_t vid, uint32_t n);
+int containsVertex(Vertex v, Vertex adj);
+void removeAdjacent(Vertex v, Vertex adj);
+void addAdjacent(Vertex v, Vertex adj);
+void printAdjacent(Vertex v);
+void reverseArcs(Vertex v);
+
+
+#endif
