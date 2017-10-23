47544fd5f84d4e20ca65c1912b53be3a59746809
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-10-23 14:18:18 +1300

    My python scripts that do all of the heavy lifting

---
+++ dfs.py
@@ -0,0 +1,9 @@
+import os
+
+
+def dfs():
+    
+    target = "make run-dfs";
+    os.system(target);
+
+dfs();
---
+++ list2reverse.py
@@ -0,0 +1,9 @@
+import os
+
+
+def list2reverse():
+    
+    target = "make run-rev";
+    os.system(target);
+
+list2reverse();
