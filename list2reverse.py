345658a96778426afd50dd45f7171e3a3b599fe9
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-10-29 18:48:36 +1300

    To please University

--- list2reverse.py
+++ list2reverse.py
@@ -1,3 +1,14 @@
+"""
+Guy Nankivell
+
+981308038
+
+This is mostly not written in Python. I prefer C so this script calls 
+a makefile, which compiles and runs the program. The relevant source
+code can be found in src/
+
+"""
+
 import os
 
 
