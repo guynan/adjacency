d25f3d316a05f1b17c511deba5d732343c24365f
Guy Nankivell <guy.nankivell@solnet.co.nz>
2017-11-24 14:36:09 +1300

    Inclusion guards for master header

--- src/adjacency.h
+++ src/adjacency.h
@@ -1,5 +1,10 @@
+#ifndef         __ADJACENCY_MASTER_ADJACENCY__
+#define         __ADJACENCY_MASTER_ADJACENCY__
+
 #include "meta.h"
 #include "vertex.h"
 #include "graph.h"
 #include "fileutils.h"
 #include "adjlist.h"
+
+#endif
